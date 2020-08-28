71041399834b20d97311fdd4e53c31a5713f535d
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-08-29 02:14:37 +0530

    ViewPager setup

--- Onboarding/app/src/main/java/com/bazukaa/onboarding/MainActivity.java
+++ Onboarding/app/src/main/java/com/bazukaa/onboarding/MainActivity.java
@@ -1,14 +1,99 @@
 package com.bazukaa.onboarding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private OnboardingAdapter onboardingAdapter;
+    private LinearLayout layoutOnboardingIndicators;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layoutOnboardingIndicators = findViewById(R.id.layoutOnboardingIndicator);
+
+        setupOnboardingItems();
+
+        ViewPager2 onboardingViewPager = findViewById(R.id.onboardingViewpager);
+        onboardingViewPager.setAdapter(onboardingAdapter);
+
+        setOnboardingIndicators();
+        setCurrentOnboardingIndicator(0);
+
+        onboardingViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentOnboardingIndicator(position);
+            }
+        });
+    }
+    private void setupOnboardingItems(){
+        List<OnboardingItem> onboardingItems = new ArrayList<>();
+
+        OnboardingItem item1 = new OnboardingItem();
+        item1.setTitle("Screen 1");
+        item1.setDescription("Description 1");
+        item1.setImage(R.drawable.o1);
+
+        OnboardingItem item2 = new OnboardingItem();
+        item2.setTitle("Screen 2");
+        item2.setDescription("Description 2");
+        item2.setImage(R.drawable.o2);
+
+        OnboardingItem item3 = new OnboardingItem();
+        item3.setTitle("Screen 3");
+        item3.setDescription("Description 3");
+        item3.setImage(R.drawable.o3);
+
+        onboardingItems.add(item1);
+        onboardingItems.add(item2);
+        onboardingItems.add(item3);
+
+        onboardingAdapter = new OnboardingAdapter(onboardingItems);
+    }
+    private void setOnboardingIndicators(){
+        ImageView[] indicators = new ImageView[onboardingAdapter.getItemCount()];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8, 0, 8, 0);
+        for(int i = 0; i < indicators.length; i++){
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.onboarding_indicator_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            layoutOnboardingIndicators.addView(indicators[i]);
+        }
+    }
+    private void setCurrentOnboardingIndicator(int index){
+        int childCount = layoutOnboardingIndicators.getChildCount();
+        for(int i = 0; i < childCount; i++){
+            ImageView imageView = (ImageView) layoutOnboardingIndicators.getChildAt(i);
+            if(i == index){
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_active)
+                );
+            }else {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_inactive)
+                );
+            }
+        }
     }
 }
+
